50f79af0f9a550f3f884276accc7ce27cfeb5d7e
Clay_MM <clay.moyer.1515@gmail.com>
2023-07-24 10:51:27 -0400

    length prompt

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//user prompts
+function lengthPrompt() {
+  var passwordLength = prompt("How many characters would you like the password to be? I offer 8-128 characters long.")
+  if(passwordLength >= 8 && passwordLength <= 128) {
+    return passwordLength
+  }
+  return lengthPrompt()
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
